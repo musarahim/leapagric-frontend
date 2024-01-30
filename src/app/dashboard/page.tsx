c3915ba3c434bb9cf1784d96ca4564057c649167
Musa Rahim <musarahim13@gmail.com>
2024-01-30 14:00:49 +0300

    added dashboard for general user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Breadcrump, Footer, Navbar } from '@/components';
+import { Metadata } from 'next';
+import Dashboard from './Dashboard';
+
+
+export const metadata: Metadata = {
+  title: "Farm Wise | Dashboard",
+  description: "Get the best for your farming needs",
+};
+
+const pages = [
+  { name: 'Home', href: '/dashboard', current: true },
+]
+
 
 export default function page() {
   return (
-    <div>
-      My dashboard
+    <>
+     <Navbar />
+    <Breadcrump pages={pages} />
+    <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>
+      <Dashboard />
     </div>
+    <Footer />
+</>
   )
 }
